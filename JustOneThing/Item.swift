054b3ca525e6b7eeaebd6f54881ce9c3c986cd09
Sam Jones <sam@samueljones.codes>
2024-09-24 23:33:06 -0700

    Initial Commit

---
+++ JustOneThing/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  JustOneThing
+//
+//  Created by Samuel Jones on 9/24/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ JustOneThingTests/JustOneThingTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustOneThingTests.swift
+//  JustOneThingTests
+//
+//  Created by Samuel Jones on 9/24/24.
+//
+
+import Testing
+@testable import JustOneThing
+
+struct JustOneThingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
